ad38874a9665f4b3d7967d2e9c65178d31ece1d8
Kohilan Mohanarajan <kohilan6@hotmail.com>
2018-10-28 12:09:12 -0400

    Updated run script to give progress info

--- commitJson.sh
+++ commitJson.sh
@@ -1,10 +1,14 @@
+echo "Running room scraper script..."
 python3 room_scraper_v2.py
 
+echo "Copying to website..."
 cp rooms.json ../mywebsite
 cd ../mywebsite 
-ls
 
+echo "Pulling from master..."
 git pull
+
+echo "Committing to master..."
 git add rooms.json
 git commit -m "Updated rooms"
 git push
